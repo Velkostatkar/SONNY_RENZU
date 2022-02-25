ca3f43936b52a25811ea887ba470b1bbe18b211b
ǝǝq-⅄uuoS <70021784+Velkostatkar@users.noreply.github.com>
2022-02-25 22:13:46 +0100

    Add files via upload

---
+++ [SONNY_RENZU]/hud_turbo_engine_bodystatus.sql
@@ -0,0 +1,32 @@
+
+CREATE TABLE IF NOT EXISTS `renzu_turbo` (
+  `plate` varchar(64) NOT NULL DEFAULT '',
+  `turbo` longtext NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `renzu_muffler` (
+  `plate` varchar(64) NOT NULL DEFAULT '',
+  `muffler` longtext NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS `vehicle_status` (
+	`stats` LONGTEXT NULL COLLATE 'utf8mb4_general_ci',
+	`plate` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	`owner` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`plate`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+CREATE TABLE IF NOT EXISTS `body_status` (
+	`status` LONGTEXT NULL COLLATE 'utf8mb4_general_ci',
+	`identifier` VARCHAR(64) NOT NULL DEFAULT '' COLLATE 'utf8mb4_general_ci',
+	PRIMARY KEY (`identifier`) USING BTREE
+)
+COLLATE='utf8mb4_general_ci'
+ENGINE=InnoDB
+;
+
